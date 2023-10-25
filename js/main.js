64248a777e080f676e40013b2475689c915db1ff
Green?Hat! <104572384+DARAZOUKI@users.noreply.github.com>
2023-10-25 18:34:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,20 +9,6 @@
 
 /* Här under börjar du skriva din JavaScript-kod */
 
-
-
-// Denna fil ska innehålla din lösning till uppgiften (moment 6).
-
-"use strict"    ;
-
-/*  Delar till ej obligatorisk funktionalitet, som kan ge poäng för högre betyg
-*   Radera rader för funktioner du vill visa på webbsidan. */
-//document.getElementById("player").style.display = "none";      // Radera denna rad för att visa musikspelare
-
-//document.getElementById("shownumrows").style.display = "none"; // Radera denna rad för att visa antal träffar
-
-/* Här under börjar du skriva din JavaScript-kod */
-
 document.addEventListener("DOMContentLoaded", function () {
 
     const playButton = document.getElementById("playbutton");
